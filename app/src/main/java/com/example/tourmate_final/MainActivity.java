3406c36cb28076d7cc683be646f5eca5897e0b64
Masum-Al-Reza <masum231987@gmail.com>
2019-12-04 00:51:36 +0600

    api setup weather

--- app/src/main/java/com/example/tourmate_final/MainActivity.java
+++ app/src/main/java/com/example/tourmate_final/MainActivity.java
@@ -1,14 +1,41 @@
 package com.example.tourmate_final;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProviders;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.tourmate_final.viewmodel.LocationViewmodel;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+public class MainActivity extends AppCompatActivity   {
+    private LocationViewmodel locationViewmodel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        locationViewmodel= ViewModelProviders.of(this).get(LocationViewmodel.class);
+        isLocationPermissionGranted();
+    }
+    private boolean isLocationPermissionGranted(){
+        if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) !=
+                PackageManager.PERMISSION_GRANTED){
+            requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 111);
+            return false;
+        }
+
+        return true;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == 111 && grantResults[0] ==
+                PackageManager.PERMISSION_GRANTED){
+            locationViewmodel.getcurrentlocation();
+        }
     }
 }
---
+++ app/src/main/java/com/example/tourmate_final/viewmodel/LocationViewmodel.java
@@ -0,0 +1,43 @@
+package com.example.tourmate_final.viewmodel;
+
+import android.app.Application;
+import android.content.Context;
+import android.location.Location;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.MutableLiveData;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
+
+import org.w3c.dom.Text;
+
+public class LocationViewmodel extends AndroidViewModel {
+    private Context context;
+    private FusedLocationProviderClient fusedLocationProviderClient;
+    public MutableLiveData<Location>locationLd=new MutableLiveData<>();
+
+    public LocationViewmodel(@NonNull Application application) {
+        super(application);
+        this.context=application;
+        fusedLocationProviderClient= LocationServices.getFusedLocationProviderClient(context);
+    }
+    public  void  getcurrentlocation(){
+        fusedLocationProviderClient.getLastLocation().addOnSuccessListener(new OnSuccessListener<Location>() {
+            @Override
+            public void onSuccess(Location location) {
+                if (location==null){
+                    return;
+
+                }
+              locationLd.postValue(location);
+                Toast.makeText(context, "found", Toast.LENGTH_SHORT).show();
+
+
+            }
+        });
+    }
+}
